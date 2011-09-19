8436b49d28ecf5b5e3c4b5e53438d6f9da22e531
Scott Petersen <petersen@dunedain289.com>
2011-09-18 23:53:15 -0500

    Initial commit
    Stubbed out Rails 3.1 + Mongoid.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Launchpad::Application.config.secret_token = '4254638ed490466b1cd109b63f3247017016c064141f8ceb06943d1a11c4edecf29c924fa2f061544ff02f14927d45f7c77a514b1369f82c95510558346b7d57'
